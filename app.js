a8dc1da3559424154e64d669710c2498f5f23d3b
natalia <zhestckova.natali@yandex.ru>
2021-09-17 02:23:07 +0300

    [task-0] heroku deployment

---
+++ app.js
@@ -0,0 +1,22 @@
+const DIST_PATH = '/dist';
+const APPLICATION_PATH = `${DIST_PATH}/nir-v0`;
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(path.join(__dirname, APPLICATION_PATH)));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, `${APPLICATION_PATH}/index.html`));
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(`${port}`, () => console.log("[APP-NIR-V0] running"));
